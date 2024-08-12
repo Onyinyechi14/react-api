65546f11325ff434f6d8cd03a0d6a39008e6bdcf
Onyinyechi14 <giftjulius070@gmail.com>
2024-08-12 13:20:02 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import UserList from './UserList';
+import './App.css'; // Add custom styles here
+
+function App() {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
